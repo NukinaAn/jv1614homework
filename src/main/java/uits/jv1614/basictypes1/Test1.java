65b7818f8f215121b1c8ad7ce96845f41a270bdb
nukina_a <nukina2694@gmail.com>
2017-02-09 17:50:40 +0200

    change Test1.java

--- src/main/java/uits/jv1614/basictypes1/Test1.java
+++ src/main/java/uits/jv1614/basictypes1/Test1.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uits.jv1614.basictypes1;
 
-/**
- *
- * @author Настя
- */
 public class Test1 {
+    public static void main(String[] args){
+        System.out.println ("Hellow, world!!!");
+        app();
+        Newtype();
+    }
     
+    public static void app(){
+         String stih = "Влесу родилась елочка\n" 
+                + "В лесу она росла"; 
+    System.out.println(stih); 
+    }
+    
+    public static void Newtype(){
+        String name = "Niukina Anastasia Nikolaevna"; 
+    String a = "jv"; 
+    int age = 1614; 
+    String b = "Wednesday - 19:00";
+    String s = "Sunday - 09:00";
+            System.out.println("My name is: " + name);
+        System.out.print("Group number: " + a);
+        System.out.println(age); 
+    System.out.println("Training time: " + b);
+    System.out.print("               " + s);
+       }
 }
+
+
